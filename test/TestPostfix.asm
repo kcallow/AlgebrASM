2ec4b3cca46262eeb34d20b1f21e0f2a0a65e4af
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-05-13 21:37:48 -0600

    Revivir Postfix a peticion de Andresito

---
+++ test/TestPostfix.asm
@@ -0,0 +1,19 @@
+%include "../Postfix.asm"
+section .text
+global _start
+_start:
+	mov	rdi, Input	;Write to input buffer
+	mov	bl, ','		;Comma is delimitor for input
+	mov	rcx,1024	;Read 1024 chars max
+	call	getString
+	call	postfix
+
+	call	copyTemp2Input
+
+        mov     rsi,Temp
+	call	print
+
+	mov	rax,60
+	mov	rdi,0
+	syscall
+;end _start
