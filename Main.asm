5e7d440b8815a72c5deb7f37658d9527c588e8fd
Kenneth Callow <kcallow@ic-itcr.ac.cr>
2015-05-18 12:38:34 -0600

    >Main parcial

---
+++ Main.asm
@@ -0,0 +1,23 @@
+%import "Postfix.asm"
+section .data
+global _start
+_start:
+	mov	rdi, Input	;Write to input buffer
+	mov	bl, ','		;Comma is delimitor for input
+	mov	rcx,21*BUFSIZE	;Read 21 buffers
+	call	getString
+	call	removeWhitespace
+	call	copyTemp2Input
+
+	call	addAsterisks
+	call	copyTemp2Input
+
+	call	getAndReplaceAllVars
+
+	call	postfix
+	call	copyTemp2Input
+
+	mov	rax,60
+	mov	rdi,0
+	syscall
+;end _start
